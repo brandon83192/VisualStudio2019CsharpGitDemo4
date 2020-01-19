8707ed24afa02a3aab28e13ebe82d79836770722
Btond <Btond@LAPTOP-K8U30UIB>
2020-01-19 16:02:12 -0500

    commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace VisualStudio2019CsharpGitDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("New change");
         }
     }
 }
